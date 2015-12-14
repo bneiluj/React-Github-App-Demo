f128ab193f5f9c28f7cb3be49fbb2b82ea28157f
bneiluj <julien.bouteloup@net-a-porter.com>
2015-12-14 20:33:43 +0000

    create simple hello world with the basic react routing

---
+++ app/config/routes.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var Main = require('../components/Main');
+var Home = require('../components/Home');
+var Router = require('react-router');
+var Route = Router.Route;
+
+module.exports = (
+  <Route path="/" component={Main}>
+  </Route>
+);
