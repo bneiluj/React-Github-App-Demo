788e110ae3665e87833b73abdfd5a56d5d96d0c3
bneiluj <julien.bouteloup@gmail.com>
2016-02-03 15:58:43 +0000

    adopt Stateless Function Components

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import SearchGithub from './SearchGithub';
 
-class Main extends React.Component {
+// save as this:
+// const Main = (props) => {
+const Main = ({history, children}) => {
     // Main component has access to this.props.history is because
     // the main component is being controlled by the router
-    render () {
-        return (
-          <div className="main-container">
-            <nav className="navbar navbar-default" role="navigation">
-              <div className="col-sm-7 col-sm-offset-2" style={{marginTop: 15}}>
-                <SearchGithub history={this.props.history}/>
-              </div>
-            </nav>
-            <div className="container">
-              {this.props.children}
-            </div>
+    return (
+      <div className="main-container">
+        <nav className="navbar navbar-default" role="navigation">
+          <div className="col-sm-7 col-sm-offset-2" style={{marginTop: 15}}>
+            <SearchGithub history={history}/>
           </div>
-        )
-    }
+        </nav>
+        <div className="container">
+          {children}
+        </div>
+      </div>
+    )
 }
 
 // Exports
--- app/components/Notes/NotesList.js
+++ app/components/Notes/NotesList.js
@@ -1,18 +1,13 @@
 import React from 'react';
 
-class NotesList extends React.Component {
-    render(){
-        // Destructuring variables
-        // Saving into a variable called notes
-        const { notes } = this.props;
-        return (
-            <ul className="list-group">
-              {notes.map((note, index)  => (
-                <li className="list-group-item" key={index}>{note}</li>
-              ))}
-            </ul>
-        )
-    }
+const NotesList = ({notes}) => {
+    return (
+        <ul className="list-group">
+          {notes.map((note, index)  => (
+            <li className="list-group-item" key={index}>{note}</li>
+          ))}
+        </ul>
+    )
 }
 
 export default NotesList;
